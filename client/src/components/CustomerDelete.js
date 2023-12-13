e3a84346d838d2fd39831db5a5960d4932e1b4b4
홍승희 <seung0476@gmail.com>
2023-12-13 18:22:35 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,7 +1,25 @@
 import React from "react";
-import { Component } from "react";
+import { Dialog,DialogActions, DialogTitle, DialogContent, Button, Typography } from "@mui/material";
 
 class CustomerDelete extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
 
     deleteCustomer(id){
         const url = '/api/customers/'+id;
@@ -13,7 +31,23 @@ class CustomerDelete extends React.Component{
 
     render(){
         return(
-            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</button>
+            <div>
+            <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle onClose={this.handleClose}>
+                    삭제 경고
+                </DialogTitle>
+                <DialogContent>
+                    <Typography gutterBottom>
+                        선택한 고객 정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
+                    <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                </DialogActions>
+            </Dialog>
+            </div>
         )
     }
 }
